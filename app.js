cb46e518f977a161bc1a53266423ab27421703f8
Sohail Haider <sohail.haider@autoscout24.com>
2023-11-28 13:17:24 +0100

    log server url in terminal for quick access

--- app.js
+++ app.js
@@ -61,5 +61,5 @@ app.use(express.static("public"));
 
 // Start the server
 app.listen(8765, () => {
-  console.log("Server is running on port 8765");
+  console.log("Server is running on port http://localhost:8765");
 });
